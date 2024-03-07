35a116488b3ea763929e6aad8c0698e4d17680b6
Vlad Grey <Vlad9rey@gmail.com>
2024-03-07 23:11:03 +0200

    add set time option

--- src/Card.jsx
+++ src/Card.jsx
@@ -7,35 +7,54 @@ import CountButton from './CountButton'
 
 export default function Card() {
     const [isActive, setIsActive] = useState(false);
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(5);
+    const [inputMinutes, setInputMinutes] = useState(5);
+    const [inputSeconds, setInputSeconds] = useState(0);
+    const [inputMinActiveChange, setInputMinActiveChange] = useState(false)
+    const [inputSecActiveChange, setInputSecActiveChange] = useState(false)
+
+    const handleChangeMinutes = (e) => {
+        const value = e.target.value;
+        if (/^\d*$/.test(value) && value.length <= 2) {
+            setInputMinutes(value);
+        }
+    }
+
+
+    const handleChangeSeconds = (e) => {
+        const value = e.target.value;
+        if (/^\d*$/.test(value) && value.length <= 2) {
+            setInputSeconds(value);
+        }
+    }
+
 
     useEffect(() => {
         let interval
         if (isActive) {
             interval = setInterval(() => {
-                if (minutes === 0 && seconds === 0) {
+                if (inputMinutes == 0 && inputSeconds == 0) {
                     clearInterval(interval)
                     setIsActive(false);
+                    console.log("timer stoped");
+                } else if (inputSeconds == 0 && inputMinutes != 0) {
+                    console.log(inputMinutes);
+                    setInputMinutes(prev => prev - 1);
+                    setInputSeconds(59);
                 } else {
-                    if (seconds === 0) {
-                        setMinutes(prev => prev - 1);
-                        setSeconds(59);
-                    } else
-                        setSeconds(prev => prev - 1)
+                    console.log(inputMinutes);
+                    setInputSeconds(prev => prev - 1)
                 }
             }, 1000);
         }
 
         return () => clearInterval(interval)
-    }, [minutes, seconds, isActive]);
-
+    }, [inputMinutes, inputSeconds, isActive]);
 
 
     const handleStop = () => {
-        setSeconds(0);
-        setMinutes(5);
         setIsActive(false);
+        setInputMinutes(0)
+        setInputSeconds(0)
     }
 
     const handlePasue = () => {
@@ -43,14 +62,50 @@ export default function Card() {
     }
 
     const handleStart = () => {
-        setIsActive(true);
+        if (inputSeconds !== 0 || inputMinutes !== 0)
+            setIsActive(true);
     }
 
     return (
         <div className={`card ${""}`}>
             <Title />
             <Count>
-                {`0${minutes}:`}{seconds > 9 ? `${seconds}` : `0${seconds}`}
+                {inputMinActiveChange ? (
+                    <>
+                        <input
+                            onChange={handleChangeMinutes}
+                            onBlur={() => setInputMinActiveChange(false)}
+                            type="number"
+                            placeholder="00"
+                            className="time"
+                            value={inputMinutes}
+                            autoFocus
+                        />
+                    </>
+                ) : (
+                    <>
+                        <span className="time" onClick={() => setInputMinActiveChange(true)}>{inputMinutes >= 10 || inputMinutes.length >= 2 ? inputMinutes : `0${inputMinutes}`}</span>
+                    </>
+                )}
+                {":"}
+                {inputSecActiveChange ? (
+                    <>
+                        <input
+                            onChange={handleChangeSeconds}
+                            onBlur={() => setInputSecActiveChange(false)}
+                            type="number"
+                            placeholder="00"
+                            className="time"
+                            value={inputSeconds}
+                            autoFocus
+                        />
+                    </>
+                ) : (
+                    <>
+                        <span className="time" onClick={() => setInputSecActiveChange(true)}>{inputSeconds >= 10 || inputSeconds.length >= 2 ? inputSeconds : `0${inputSeconds}`}</span>
+                    </>
+                )}
+
             </Count>
 
             <ResetButton onClick={handleStop} />
